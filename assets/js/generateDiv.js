e933ec0496023a8ad9e7b78d5f985364efa51b82
de-mark <data.engineer.mark@gmail.com>
2023-05-17 11:44:56 -0700

    Added step 1 and 2 of eui-64

--- assets/js/generateDiv.js
+++ assets/js/generateDiv.js
@@ -47,7 +47,14 @@ const addNewMACID = () => {
 }
 
 const generateEUI64 = (cleanedMacAddress) => {
-    return cleanedMacAddress;
+    // EUI-64 STEP 1: Split the 48-bit MAC address down the middle
+    let OUI = cleanedMacAddress.slice(0, 3);
+    let deviceID = cleanedMacAddress.slice(3, 6);
+
+    // EUI-64 STEP 2: The host puts "fffe", a special reserved value between these two addresses
+    let newMacAddress = [...OUI, "FF", "FE", ...deviceID];
+
+    return newMacAddress.join(":");
 }
 
 const addNewEUI64 = () => {
@@ -56,5 +63,10 @@ const addNewEUI64 = () => {
     rawMacAddress = rawMacAddress.replace(/[.,\/#!$%+\^&\*;:{}=\-_`~()]/g,":");
     let cleanedMacAddress = rawMacAddress.split(":");
 
-    document.getElementById("generatedID").innerHTML = generateEUI64(cleanedMacAddress);
+    if (cleanedMacAddress.length == 6){
+        document.getElementById("errorDiv").innerHTML = "";
+        document.getElementById("generatedID").innerHTML = generateEUI64(cleanedMacAddress);
+    } else {
+        document.getElementById("errorDiv").innerHTML = `<b>Error:</b> A MAC Address must have 6 Octets; Yours has ${cleanedMacAddress.length}`;
+    }  
 }
